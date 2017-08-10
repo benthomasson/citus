c130f1852ae520d9d93eb33f714b625dcc8bc1a8
Brian Cloutier <github@briancloutier.com>
2017-08-10 12:37:31 +0300

    Break ALTER SYSTEM into separate test

--- src/test/regress/sql/multi_follower_select_statements.sql
+++ src/test/regress/sql/multi_follower_select_statements.sql
@@ -37,14 +37,7 @@ SELECT 1 FROM master_add_node('localhost', :follower_worker_2_port,
   groupid => (SELECT groupid FROM pg_dist_node WHERE nodeport = :worker_2_port),
   noderole => 'secondary');
 
--- (the previous \c is a good example of how to change a variable temporarily and left
--- in for future reference but from now on we always want the follower to read from
--- secondaries so permanently change it. use_secondary_nodes is SU_BACKEND so you
--- must reconnect in order for the change to take effect)
-\c - - - :follower_master_port
-ALTER SYSTEM SET citus.use_secondary_nodes TO 'always';
-SELECT pg_reload_conf();
-\c - - - -
+\c "port=57700 dbname=regression options='-c\ citus.use_secondary_nodes=always'"
 
 -- now that we've added secondaries this should work
 SELECT * FROM the_table;
@@ -52,10 +45,7 @@ SELECT * FROM the_table;
 -- okay, now let's play with nodecluster. If we change the cluster of our follower node
 -- queries should stat failing again, since there are no worker nodes in the new cluster
 
--- cluster_name is also SU_BACKEND, so we need to reconnect after changing it
-ALTER SYSTEM SET citus.cluster_name TO 'second-cluster';
-SELECT pg_reload_conf();
-\c - - - -
+\c "port=57700 dbname=regression options='-c\ citus.use_secondary_nodes=always\ -c\ citus.cluster_name=second-cluster'"
 
 -- there are no secondary nodes in this cluster, so this should fail!
 SELECT * FROM the_table;
@@ -65,5 +55,9 @@ SELECT * FROM the_table;
 
 \c - - - :master_port
 UPDATE pg_dist_node SET nodecluster = 'second-cluster' WHERE noderole = 'secondary';
-\c - - - :follower_master_port
+\c "port=57700 dbname=regression options='-c\ citus.use_secondary_nodes=always\ -c\ citus.cluster_name=second-cluster'"
 SELECT * FROM the_table;
+
+-- clean up after ourselves
+\c - - - :master_port
+DROP TABLE the_table;
