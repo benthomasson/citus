b6d8832729356eae078b524c773ee5f4d18c01d1
Brian Cloutier <github@briancloutier.com>
2017-08-10 11:00:44 +0300

    Rename :follower_port -> :follower_master_port

--- src/test/regress/sql/multi_follower_sanity_check.sql
+++ src/test/regress/sql/multi_follower_sanity_check.sql
@@ -1,6 +1,6 @@
 -- check that the nodes are all in read-only mode and rejecting write queries
 
-\c - - - :follower_port
+\c - - - :follower_master_port
 CREATE TABLE tab (a int);
 \c - - - :follower_worker_1_port
 CREATE TABLE tab (a int);
--- src/test/regress/sql/multi_follower_select_statements.sql
+++ src/test/regress/sql/multi_follower_select_statements.sql
@@ -14,14 +14,14 @@ INSERT INTO the_table (a, b) VALUES (1, 2);
 -- connect to the follower and check that a simple select query works, the follower
 -- is still in the default cluster and will send queries to the primary nodes
 
-\c - - - :follower_port
+\c - - - :follower_master_port
 
 SELECT * FROM the_table;
 
 -- now, connect to the follower but tell it to use secondary nodes. There are no
 -- secondary nodes so this should fail.
 
--- (this is :follower_port but substitution doesn't work here)
+-- (this is :follower_master_port but substitution doesn't work here)
 \c "port=57700 dbname=regression options='-c\ citus.use_secondary_nodes=always'"
 
 SELECT * FROM the_table;
@@ -41,7 +41,7 @@ SELECT 1 FROM master_add_node('localhost', :follower_worker_2_port,
 -- in for future reference but from now on we always want the follower to read from
 -- secondaries so permanently change it. use_secondary_nodes is SU_BACKEND so you
 -- must reconnect in order for the change to take effect)
-\c - - - :follower_port
+\c - - - :follower_master_port
 ALTER SYSTEM SET citus.use_secondary_nodes TO 'always';
 SELECT pg_reload_conf();
 \c - - - -
@@ -65,5 +65,5 @@ SELECT * FROM the_table;
 
 \c - - - :master_port
 UPDATE pg_dist_node SET nodecluster = 'second-cluster' WHERE noderole = 'secondary';
-\c - - - :follower_port
+\c - - - :follower_master_port
 SELECT * FROM the_table;
